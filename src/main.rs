cc12136068c232c7122b689e4555ceda6ed71313
June B <jrbergeron823@gmail.com>
2024-11-16 14:22:39 -0600

    Add command line input for getting children & more
    Incorporated stdin for reading what directory the user wants to get the children of
    Simplify each return of std::io::Result<...> to just io::Result<...>
    Probably more that I'm forgetting right now, but oh well

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 // #![allow(unused_imports)]
 use std::ffi::OsString;
 use std::fs::{self, ReadDir};
-// use std::io::Result;
+use std::io::{self};
 use std::path::Path;
 
-fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
 
-    let current_dir: &Path = Path::new(".");
+    println!("What directory?");
 
-    for i in get_children(current_dir)?.iter() {
-        println!("{:#?}", i.as_os_str().to_str().unwrap())
+    let mut input = String::new();
+    stdin.read_line(&mut input)?;
+
+    let input_dir: &Path = Path::new(input.trim());
+
+    if !input_dir.exists() {
+        panic!("Directory does not exist");
+    }
+
+    let dir_str = &input_dir.to_str().unwrap();
+
+    println!("{}'s children:", dir_str);
+
+    for i in get_children(input_dir)?.iter() {
+        println!("{}", i.to_str().unwrap())
     }
 
     Ok(())
 }
 
-fn get_children(dir: &Path) -> std::io::Result<Vec<OsString>> {
+fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
     if !dir.exists() {
-        panic!("directory does not exist");
+        panic!("Path does not exist");
     }
 
     let entries: ReadDir = fs::read_dir(dir)?;
