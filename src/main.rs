c5cc6e8dfc6ace5fbda5f6eca85e4efb5f587281
June B <jrbergeron823@gmail.com>
2024-11-17 23:42:51 -0600

    Add formatting function & others
    Move from asking for separate input to just reading from CL right after 'cargo run'
    Add function that (is supposed to) formats paths, but it only actually works if the input is './'
    Edit some println!() calls for debugging
    And some more things that I'm forgetting

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
 // #![allow(unused_imports)]
+// use std::env::args;
 use std::ffi::OsString;
 use std::fs::{self, ReadDir};
 use std::io::{self};
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin();
+    let arguments: Vec<String> = std::env::args().collect();
+    let input_path: &Path = Path::new(arguments.get(1).unwrap());
 
-    println!("What directory?");
-
-    let mut input = String::new();
-    stdin.read_line(&mut input)?;
-
-    let input_dir: &Path = Path::new(input.trim());
-
-    if !input_dir.exists() {
-        panic!("Directory does not exist");
+    if !input_path.exists() {
+        panic!("Path does not exist (a)");
     }
 
-    let dir_str = &input_dir.to_str().unwrap();
+    // println!("Your arguments were {:?}", arguments);
 
-    println!("{}'s children:", dir_str);
+    println!("{:#?}'s formatted children:", input_path);
 
-    for i in get_children(input_dir)?.iter() {
-        println!("{}", i.to_str().unwrap())
+    for i in get_children(input_path)?.iter() {
+        let i_as_path: &Path = Path::new(i);
+
+        // ERROR: only formats if input_path is "./"
+        match fmt_child_type(i_as_path) {
+            Some(path) => println!("{}", path.to_str().unwrap()),
+            None => panic!("Wasn't expecting this"),
+        };
     }
 
     Ok(())
 }
 
+#[allow(dead_code)]
 fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
     if !dir.exists() {
-        panic!("Path does not exist");
+        panic!("Path does not exist (b)");
     }
 
     let entries: ReadDir = fs::read_dir(dir)?;
@@ -39,7 +41,33 @@ fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
     for entry in entries {
         let entry = entry?;
         let dir_name: OsString = entry.file_name();
-        children.push(dir_name)
+        children.push(dir_name);
     }
+
     Ok(children)
+}
+
+#[allow(dead_code)]
+// entry *needs* to be &Path
+// ERROR: only formats if input_path is "./"
+fn fmt_child_type(entry: &Path) -> Option<OsString> {
+    // this was making it panic even when the entry absolutely exists
+    // if !entry.exists() {
+    //     panic!("Path does not exist (c)");
+    // }
+
+    let mut name: OsString = match entry.file_stem() {
+        Some(i) => i.to_os_string(),
+        None => panic!("Could not read path"),
+    };
+
+    if entry.is_file() {
+        return Some(entry.file_name().unwrap().to_os_string());
+    } else if entry.is_dir() {
+        name.push("/");
+    } else if entry.is_symlink() {
+        name.push("@");
+    }
+
+    Some(name)
 }
