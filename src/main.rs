507f5aeb3330d39f6c399f13027b86638e97ccb2
June B <jrbergeron823@gmail.com>
2024-11-22 01:39:10 -0600

    Rewrite format function and some other stuff (idk im tired)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 // #![allow(unused_imports)]
-// use std::env::args;
+use std::env;
 use std::ffi::OsString;
 use std::fs::{self, ReadDir};
 use std::io::{self};
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let arguments: Vec<String> = std::env::args().collect();
+    let arguments: Vec<String> = env::args().collect();
     let input_path: &Path = Path::new(arguments.get(1).unwrap());
+    // let input_path: Vec<&Path> = arguments[1..].iter().map(|a| Path::new(a)).collect();
 
-    if !input_path.exists() {
-        panic!("Path does not exist (a)");
-    }
+    // if !input_path.exists() {
+    //     panic!("Path does not exist (a)");
+    // }
 
     // println!("Your arguments were {:?}", arguments);
 
@@ -20,17 +21,17 @@ fn main() -> io::Result<()> {
     for i in get_children(input_path)?.iter() {
         let i_as_path: &Path = Path::new(i);
 
-        // ERROR: only formats if input_path is "./"
-        match fmt_child_type(i_as_path) {
-            Some(path) => println!("{}", path.to_str().unwrap()),
-            None => panic!("Wasn't expecting this"),
-        };
+        /*
+         * BUG: only formats if input_path is "./"
+         * maybe because it's not the full relative path (just the child's relative path to its parent?????)
+         */
+        println!("{:#?}", format_pls(i_as_path));
     }
 
     Ok(())
 }
 
-#[allow(dead_code)]
+// #[allow(dead_code)]
 fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
     if !dir.exists() {
         panic!("Path does not exist (b)");
@@ -47,27 +48,22 @@ fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
     Ok(children)
 }
 
-#[allow(dead_code)]
 // entry *needs* to be &Path
-// ERROR: only formats if input_path is "./"
-fn fmt_child_type(entry: &Path) -> Option<OsString> {
-    // this was making it panic even when the entry absolutely exists
-    // if !entry.exists() {
-    //     panic!("Path does not exist (c)");
-    // }
-
-    let mut name: OsString = match entry.file_stem() {
-        Some(i) => i.to_os_string(),
-        None => panic!("Could not read path"),
-    };
-
-    if entry.is_file() {
-        return Some(entry.file_name().unwrap().to_os_string());
-    } else if entry.is_dir() {
-        name.push("/");
-    } else if entry.is_symlink() {
-        name.push("@");
+fn format_pls(entry: &Path) -> OsString {
+    if entry.is_dir() && !ends_with(entry, '/') {
+        let fmt_dir = format!("{}/", entry.display());
+        return OsString::from(fmt_dir);
     }
 
-    Some(name)
+    OsString::from(entry)
+}
+
+fn ends_with(dir: &Path, foo: char) -> bool {
+    let length = OsString::from(dir).len();
+    let bar = match dir.to_str() {
+        Some(i) => i.as_bytes(),
+        None => panic!("Couldn't convert to an OsString"),
+    };
+
+    bar[length - 1] as char == foo
 }
