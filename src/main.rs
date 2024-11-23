c656532d942a207b3e40121754f77c4ec4d446f1
June B <jrbergeron823@gmail.com>
2024-11-22 22:39:14 -0600

    Rename poorly named / undescriptive vars, func names, etc.

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,49 @@ use std::path::Path;
 fn main() -> io::Result<()> {
     let arguments: Vec<String> = env::args().collect();
     let input_path: &Path = Path::new(arguments.get(1).unwrap());
-    // let input_path: Vec<&Path> = arguments[1..].iter().map(|a| Path::new(a)).collect();
 
-    // if !input_path.exists() {
-    //     panic!("Path does not exist (a)");
-    // }
+    if !input_path.exists() {
+        panic!("Path does not exist (a)");
+    }
 
-    // println!("Your arguments were {:?}", arguments);
+    if arguments.len() != 2 {
+        panic!("Unexpected number of arguments.\nPlease enter just the path");
+    }
 
-    println!("{:#?}'s formatted children:", input_path);
+    println!("{}'s formatted children:", input_path.display());
 
     for i in get_children(input_path)?.iter() {
         let i_as_path: &Path = Path::new(i);
 
-        /*
-         * BUG: only formats if input_path is "./"
+        /* BUG: only formats if input_path is "./"
          * maybe because it's not the full relative path (just the child's relative path to its parent?????)
          */
-        println!("{:#?}", format_pls(i_as_path));
+        println!("{}", format_path(i_as_path).to_str().unwrap());
     }
 
     Ok(())
 }
 
 // #[allow(dead_code)]
-fn get_children(dir: &Path) -> io::Result<Vec<OsString>> {
-    if !dir.exists() {
+fn get_children(parent: &Path) -> io::Result<Vec<OsString>> {
+    if !parent.exists() {
         panic!("Path does not exist (b)");
     }
 
-    let entries: ReadDir = fs::read_dir(dir)?;
+    let entries: ReadDir = fs::read_dir(parent)?;
     let mut children: Vec<OsString> = Vec::new();
     for entry in entries {
         let entry = entry?;
-        let dir_name: OsString = entry.file_name();
-        children.push(dir_name);
+        let child_name: OsString = entry.file_name();
+        children.push(child_name);
     }
 
     Ok(children)
 }
 
 // entry *needs* to be &Path
-fn format_pls(entry: &Path) -> OsString {
-    if entry.is_dir() && !ends_with(entry, '/') {
+fn format_path(entry: &Path) -> OsString {
+    if entry.is_dir() && !ends_with_char(entry, '/') {
         let fmt_dir = format!("{}/", entry.display());
         return OsString::from(fmt_dir);
     }
@@ -58,12 +58,12 @@ fn format_pls(entry: &Path) -> OsString {
     OsString::from(entry)
 }
 
-fn ends_with(dir: &Path, foo: char) -> bool {
+fn ends_with_char(dir: &Path, end_char: char) -> bool {
     let length = OsString::from(dir).len();
-    let bar = match dir.to_str() {
+    let dir_as_bytes = match dir.to_str() {
         Some(i) => i.as_bytes(),
         None => panic!("Couldn't convert to an OsString"),
     };
 
-    bar[length - 1] as char == foo
+    dir_as_bytes[length - 1] as char == end_char
 }
