4617946e126a7295c7f25656866cc5c9651d7364
June B <jrbergeron823@gmail.com>
2024-11-25 22:12:21 -0600

    Improve error handling regarding # of args, format comments

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,19 @@ fn main() -> io::Result<()> {
     let arguments: Vec<String> = env::args().collect();
     let input_path: &Path = Path::new(arguments.get(1).unwrap());
 
+    match arguments.len() {
+        0 => panic!("What the hell?!"),
+        1 => {
+            panic!("Please enter the path you want to apply to this program");
+            // ask for input again instead of panicking???
+        }
+        2 => (),
+        3.. => panic!("Unexpected number of arguments\nPlease enter just the path"),
+    }
+
     if !input_path.exists() {
         panic!("Path does not exist (a)");
     }
-
-    if arguments.len() != 2 {
-        panic!("Unexpected number of arguments.\nPlease enter just the path");
-    }
-
-    // println!("{}'s formatted children:", input_path.display());
 
     println!(
         "Your path formatted is: {}",
@@ -65,7 +69,8 @@ fn get_children(parent: &Path) -> io::Result<Vec<OsString>> {
     Ok(children)
 }
 
-// entry *needs* to be &Path
+// uncomment commented lines, and remove both `return`'s to instead return a `&Path`
+// entry *needs* to be `&Path`
 fn format_if_dir(entry: &Path) -> OsString {
     if entry.is_dir() && !ends_with_char(entry, '/') {
         // let fmt_dir: &Path = Path::new(format!("{}/", entry.display()));
@@ -92,21 +97,22 @@ fn ends_with_char(dir: &Path, end_char: char) -> bool {
     dir_as_bytes[length - 1] as char == end_char
 }
 
-// I just got tired of calling x.to_str().unwrap() a lot on OsStrings
-// I thought there were a lot more of those calls, but idk
 trait DisplayOsString {
     fn display(&self) -> &str;
     fn as_path(&self) -> &Path;
+    // similar to `.unwrap_or_else()` for `Option<T>`'s
     // fn display_or_else<T>(&self, f: FnOnce() -> T) -> T;
 }
 
 impl DisplayOsString for OsString {
+    // I just got tired of calling `x.to_str().unwrap()` a lot on `OsString`'s
+    // I thought there were a lot more of those calls, but idk
     fn display(&self) -> &str {
         self.to_str()
             .unwrap_or_else(|| panic!("Couldn't convert OsString to &str"))
     }
 
-    // solely to make format_if_dir work
+    // solely to make `format_if_dir` work
     fn as_path(&self) -> &Path {
         // Path::new(self.as_os_str())
         Path::new(self)
